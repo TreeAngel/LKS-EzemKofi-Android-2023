fe03c419acdf8c5ced1d4ee99bc9accab2f695f6
ky <nadhifmusyafa23@gmail.com>
2025-02-06 13:25:24 +0700

    commit & push

--- app/src/main/java/com/example/ezemkofi/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/ezemkofi/activity/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ezemkofi.activity
 
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.ezemkofi.R
 import com.example.ezemkofi.databinding.ActivityRegisterBinding
 import com.example.ezemkofi.util.api.viewModel.AuthViewModel
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -21,7 +23,7 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         viewModel = ViewModelProvider(this@RegisterActivity)[AuthViewModel::class.java]
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -32,7 +34,29 @@ class RegisterActivity : AppCompatActivity() {
             finish()
         }
         binding.btnRegister.setOnClickListener {
-            Toast.makeText(this@RegisterActivity, "Test", Toast.LENGTH_SHORT).show()
+            val username = binding.etUsername.text.toString().trim()
+            val fullname = binding.etFullname.text.toString().trim()
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            val confirmPassword = binding.etConfirmPassword.text.toString().trim()
+            if (username.isEmpty() || fullname.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(
+                    this@RegisterActivity,
+                    "Fill all data to create an account",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this@RegisterActivity, "Invalid email address", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            if (confirmPassword != password) {
+                Toast.makeText(this@RegisterActivity, "Confirm your password", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
         }
     }
 }
